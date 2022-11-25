20679148f88ae7ac5b56dc34f53e5017a0e85393
Maciel Ribeiro de Oliveira <38005099+Maxnegrao33@users.noreply.github.com>
2022-11-25 11:13:55 -0300

    Add files via upload

---
+++ Iniciando.js
@@ -0,0 +1,5 @@
+//Cria uma pequena caixa com um texto e um botão sobre a tela
+alert("Iniciando, Estou funcionando")
+
+//Interessante enquanto trabalha como JavaScript, deixar o Console aberto
+console.log("Olá Mundo!")
